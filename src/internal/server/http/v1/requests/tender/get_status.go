83147df3d588f8a98365e35632d71d0ed7fea80a
sv3tlov <67441287+SV3TLuV@users.noreply.github.com>
2024-09-16 09:32:45 +0300

    fix get status request

--- src/internal/server/http/v1/requests/tender/get_status.go
+++ src/internal/server/http/v1/requests/tender/get_status.go
@@ -4,5 +4,5 @@ import "github.com/google/uuid"
 
 type GetStatusRequest struct {
 	TenderID uuid.UUID `json:"tenderId" validate:"required,uuid,max=100"`
-	Username string    `query:"username" validate:"required,max=50"`
+	Username string    `query:"username" validate:"max=50"`
 }
